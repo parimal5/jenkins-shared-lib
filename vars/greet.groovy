34750fc7db6ff659180a5ec7f7b07a134aeb7878
Parimal <parimalmeshram15@gmail.com>
2024-12-03 13:57:50 +0530

    Added new function.

--- vars/greet.groovy
+++ vars/greet.groovy
@@ -1,4 +1,7 @@
-def call(String name = 'User') {
+def greet(String name = 'User') {
     echo "Hello, ${name}! Welcome to Jenkins Shared Libraries."
+}
+def goodbye(String name = 'User') {
+    echo "Hello, ${name}! See you soon."
 }
 
