469d7e4fd99a15ad490c2a7bf11cfe3b41808ef1
Parimal <parimalmeshram15@gmail.com>
2024-12-03 14:22:09 +0530

    Added Src and Lib

---
+++ src/Greeter.groovy
@@ -0,0 +1,11 @@
+package com.mycompany
+
+class Greeter {
+    static void greet(String name) {
+        echo "Hello, ${name} from Greeter class!"
+    }
+
+    static void goodbye(String name) {
+        echo "Goodbye, ${name} from Greeter class!"
+    }
+}
--- vars/MyFunction.groovy
+++
@@ -1,7 +0,0 @@
-def greetUser(String name = 'User') {
-    echo "Hello, ${name}! Welcome to Jenkins Shared Libraries. This is greet Function from MyFunction.groovy"
-}
-def goodbye(String name = 'User') {
-    echo "Hello, ${name}! See you soon."
-}
-
